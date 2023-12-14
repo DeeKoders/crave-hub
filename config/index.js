45a437a678df2f336e020570da91883dade41256
danyal-deekoders <deekoders@gmail.com>
2023-12-14 16:11:36 +0500

    convict setup for keys

--- config/index.js
+++ config/index.js
@@ -53,10 +53,31 @@ var config = convict({
       env: "DATABASE_PASSWORD",
     },
   },
+  aws: {
+    accessKeyId: {
+      doc: "AWS user access key id",
+      format: String,
+      default: "",
+      env: "AWS_ACCESS_KEY_ID",
+    },
+    secretAccessKey: {
+      doc: "AWS user secret access key",
+      format: String,
+      default: "",
+      env: "AWS_SECRET_ACCESS_KEY",
+    },
+    default_bucket: {
+      doc: "AWS S3 default bucket",
+      format: String,
+      default: "",
+      env: "AWS_BUCKET_NAME",
+    },
+  },
 });
 
 // Load environment dependent configuration
 let env = config.get("env");
+console.log("env", env);
 if (env === "development" || env === "test") {
   config.loadFile(__dirname + "/environments/" + env + ".json");
 }
