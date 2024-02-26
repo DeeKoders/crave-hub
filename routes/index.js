c927508130ea014f997e5775e2afa4477df22f20
danyal-deekoders <deekoders@gmail.com>
2024-02-26 11:33:35 +0500

    styling

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const productsRouter = require("./products.js");
 const userRouter = require("./user.js");
 
-
 router.use("/products", productsRouter);
 router.use("/users", userRouter);
-
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const userController = require("../controllers/users.js");
 
-
 router.route("/registration").post(userController.registration);
 router.route("/login").post(userController.login);
 
